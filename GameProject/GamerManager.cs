73aded23a1ffec8b8cfe272634893f689a9e15c9
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-02-10 19:19:43 +0300

    Add project files.

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if(_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayıt oluşturuldu");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama hatası! Kayıt başarısız");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi");
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1997 && gamer.FirstName == "Tamer Batuhan"
+                && gamer.LastName == "ŞEN" && gamer.IdentityNumber == 12345678912)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
